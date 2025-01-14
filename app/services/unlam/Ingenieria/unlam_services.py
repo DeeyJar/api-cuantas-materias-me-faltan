8c1df158aa46b0b177fda7cf5d69dc0611dd40be
julian redes <julian.redes@outlook.es>
2025-01-13 22:59:07 -0300

    feat: filtros para unlam2

--- app/services/unlam/Ingenieria/unlam_services.py
+++ app/services/unlam/Ingenieria/unlam_services.py
@@ -1,42 +1,54 @@
 from flask_restx import Resource
-from app.utils import BASE_DIR_CARRERA, Response_ok, Response_NotFound
-import pdfplumber
+from app.utils import BASE_DIR_CARRERA, Open_PDF, Response_NotFound
+
+UNLAM_FILTER_ING_INFORMATICA = ('', None,
+                        'PRIMER CUATRIMESTRE',
+                        'SEGUNDO CUATRIMESTRE',
+                        'semanales',
+                        'Título Intermedio: Técnico Universitario en Desarrollo de Software',
+                        'Para su obtención se requiere tener aprobadas todas las asignaturas de los tres primeros años\ninclusive, niveles I y II de Inglés Transversal',
+                        'CONOCIMIENTOS COMUNES REQUERIDOS POR HCS DE LA UNLaM',
+                        'Código',
+                        'Asignatura',
+                        'Correlatividad',
+                        'Horas',
+                        'Horas\nsemanales')
+
+UNLAM_FILTER_ING_CIVIL = ('', None,'Código','Asignatura','Correlatividad','Horas','Régimen','Cuatrimestral')
+
+UNLAM_FILTER_ING_ARQUITECTURA = ('',None,'Código','Asignatura','Correlatividad','Horas semanales')
+
+UNLAM_FILTER_ING_MECANICA = ('',None,'Código','Asignatura','Correlatividad','Horas semanales','Horas cuatrimestrales',
+                            'Total de horas anuales de transversales','384')
+
+UNLAM_FILTER_ING_ELETRONICA = ('',None)
+
+UNLAM_FILTER_ING_INDUSTRIAL = ('',None)
 
 class UNLAMIngenieria(Resource):
     def get(self,unlam_carrera):
-        try:
-            pdf_path = BASE_DIR_CARRERA + '\\carrera\\unlam\\' + unlam_carrera + '.pdf'
-            pdf = pdfplumber.open(pdf_path)
-            table = []
-            for i in pdf.pages:
-                table.append(i.extract_tables())
+        pdf_path = BASE_DIR_CARRERA + '\\carrera\\unlam\\'
+        if unlam_carrera == 'informatica':
+            pdf_path = pdf_path + "565_PlanInformatica2023.pdf"
+            data = Open_PDF(pdf_path, UNLAM_FILTER_ING_INFORMATICA)
+        elif unlam_carrera == 'civil':
+            pdf_path = pdf_path + "35_PlanCivil.pdf"
+            data = Open_PDF(pdf_path, UNLAM_FILTER_ING_CIVIL)
+        elif unlam_carrera == 'arquitectura':
+            pdf_path = pdf_path + "441_PlanArquitectura.pdf"
+            data = Open_PDF(pdf_path, UNLAM_FILTER_ING_ARQUITECTURA)
+        elif unlam_carrera == 'mecanica':
+            pdf_path = pdf_path + "488_PlanIngenieraMecnica.pdf"
+            data = Open_PDF(pdf_path, UNLAM_FILTER_ING_MECANICA)
+        elif unlam_carrera == 'eletronica':
+            pdf_path = pdf_path + "566_PlanElectronica2023.pdf"
+            data = Open_PDF(pdf_path, UNLAM_FILTER_ING_ELETRONICA)
+        elif unlam_carrera == 'industrial':
+            pdf_path = pdf_path + "700_PlanIndustrial2024.pdf"
+            data = Open_PDF(pdf_path, UNLAM_FILTER_ING_INDUSTRIAL)
+        else:
+            data = Response_NotFound()
+        
+        return data
 
-            data = []
-            for lista in table:
-                for sublista in lista:
-                    for element in sublista:
-                        filter_data = [elemento.replace('\n', ' ') for elemento in element 
-                                    if elemento not in ('', 
-                                                None,
-                                                'PRIMER CUATRIMESTRE',
-                                                'SEGUNDO CUATRIMESTRE',
-                                                'semanales',
-                                                'Título Intermedio: Técnico Universitario en Desarrollo de Software',
-                                                'Para su obtención se requiere tener aprobadas todas las asignaturas de los tres primeros años\ninclusive, niveles I y II de Inglés Transversal',
-                                                'CONOCIMIENTOS COMUNES REQUERIDOS POR HCS DE LA UNLaM',
-                                                'Código',
-                                                'Asignatura',
-                                                'Correlatividad',
-                                                'Horas',
-                                                'Horas\nsemanales',
-                                                )]
-                        if len(filter_data) > 1:
-                            data.append(filter_data)
 
-            return  Response_ok(data), 200
-        except FileNotFoundError:
-            return Response_NotFound("El pdf no se encontro") , 404
-        except IndexError:
-            return Response_NotFound("El pdf se encuentra vacio") , 404
-        except	Exception as e:
-            return Response_NotFound(e), 404
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,18 +1,51 @@
 import os
+import pdfplumber
+from flask import jsonify, make_response
 
 BASE_DIR_CARRERA = os.path.dirname(os.path.abspath(__file__))
 
 def Response_ok(data):
-    return { 
+    response = {
         'statusCode': 200, 
-        'message': 'Se encontro el listado de materias.',
-        'data' : data
+        'message': 'Se encontró el listado de materias.',
+        'data': data
     }
+    return make_response(jsonify(response), 200)
 
+def Response_Error(error):
+    response = { 
+        'statusCode': 400, 
+        'message': error
+    }
+    return make_response(jsonify(response), 400)
 
-def Response_NotFound(error):
-    return { 
+def Response_NotFound():
+    response = { 
         'statusCode': 404, 
-        'message': 'No se encontro la carrera solicitada.',
-        'messageError': error,
+        'message': 'No se encontro la carrera solicitada.'
     }
+    return make_response(jsonify(response), 404)
+
+def Open_PDF(path, filter):
+    try:
+        pdf = pdfplumber.open(path)
+        table = []
+        for i in pdf.pages:
+            table.append(i.extract_tables())
+
+        data = []
+        for lista in table:
+            for sublista in lista:
+                for element in sublista:
+                    filter_data = [elemento.replace('\n', ' ') for elemento in element 
+                                if elemento not in filter]
+                    if len(filter_data) > 1:
+                        data.append(filter_data)
+
+        return  Response_ok(data)
+    except FileNotFoundError:
+        return Response_Error("El pdf no se encontro")
+    except IndexError:
+        return Response_Error("El pdf se encuentra vacio")
+    except	Exception as e:
+        return Response_Error(e)
